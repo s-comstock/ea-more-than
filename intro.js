a222d6ab5e1074f29c9f7edeeeeb9420ff6b4a97
Sam Comstock <31906344+s-comstock@users.noreply.github.com>
2026-08-19 19:19:15 -0400

    Updating intro

---
+++ intro.js
@@ -0,0 +1,37 @@
+/*--------------------
+Intro Scroll Text JS
+--------------------*/
+
+gsap.registerPlugin(ScrollTrigger, SplitText)
+
+function initIntro() {
+  function isMobile() {
+    return window.innerWidth <= 767
+  }
+
+  const introComponent = document.querySelectorAll("[data-intro-component]")
+  if (!introComponent) return
+
+  introComponent.forEach(component => {
+    const text = component.querySelector("[data-intro-text]")
+
+    const split = new SplitText(text, { types: "words" })
+
+    const tl = gsap.timeline()
+
+    // Define different start and end values for mobile devices
+    let startValue = isMobile() ? "top 35%" : "top center"
+    let endValue = isMobile() ? "bottom 90%" : "bottom center"
+
+    tl.from(split.words, {
+      opacity: 0.25,
+      stagger: 0.1,
+      scrollTrigger: {
+        trigger: "[data-intro-component]",
+        start: startValue,
+        end: endValue,
+        scrub: 2
+      }
+    })
+  })
+}
